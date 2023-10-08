ff4500a9cde547748c9674e48c5cab10f0d155a0
L3rpio <leroyphan39@gmail.com>
2023-10-07 23:36:34 -0500

    got rid of line that clears local storage

--- js/main.js
+++ js/main.js
@@ -119,5 +119,4 @@ themeButton.addEventListener("click", () => {
   // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
-  localStorage.clear();
 });
